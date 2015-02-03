1dbd781f833cdde21bc9fe019c022d168fbf0603
David Lawrence <dkl@mozilla.com>
2015-02-03 10:40:28 -0500

    Added TimeDate to deps for sanity tests

--- travis.sh
+++ travis.sh
@@ -18,7 +18,7 @@ alias cpanm='cpanm --quiet --notest --reinstall'
 if [ "$TEST_SUITE" = "sanity" ]; then
     echo -en 'travis_fold:start:perl_dependencies\r'
     echo "== Installing Perl dependencies"
-    cpanm DateTime
+    cpanm TimeDate 
     cpanm Pod::Coverage
     echo -en 'travis_fold:end:perl_dependencies\r'
 
